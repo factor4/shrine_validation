270c4a4eaab9bf8e93eec51e125ccd0670143745
Gerald Mueller <gerald.mueller@factor4.at>
2023-03-08 15:13:26 +0100

    Test commands with Rails 6.1.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,6 +15,7 @@ class PictureUploader < Shrine
   plugin :store_dimensions, analyzer: :mini_magick
 end
 
-# Neuer Datensatz, by_admin: checked or not
+# Neuer od. existierender Datensatz, by_admin: checked or not
+# In Rails 6.0 and 6.1
 # record -> #<Gallery:0x00007feb53a27628 id: nil, name: "Test New", file_data: "{\"id\":\"3ca4e2a220a631d8c0a0147dcdbadf32.svg\",\"storage\":\"cache\",\"metadata\":{\"filename\":\"test.svg\",\"size\":3124,\"mime_type\":\"image/svg+xml\",\"width\":72,\"height\":48}}", ...>
 # record.by_admin -> nil # always
